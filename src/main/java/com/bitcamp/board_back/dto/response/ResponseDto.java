cc0141e2e735de1918a6a42849e642cefa7d868f
tjftjrgus777 <tjftjrgus123@gmail.com>
2024-06-14 11:30:23 +0900

    Feat: ResponseDto 생성

---
+++ src/main/java/com/bitcamp/board_back/dto/response/ResponseDto.java
@@ -0,0 +1,24 @@
+package com.bitcamp.board_back.dto.response;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.bitcamp.board_back.common.ResponseCode;
+import com.bitcamp.board_back.common.ResponseMessage;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ResponseDto {
+    
+    private String code;
+    private String message;
+
+    public static ResponseEntity<ResponseDto> databaseError(){
+        ResponseDto responseBody = new ResponseDto(ResponseCode.DATABASE_ERROR, ResponseMessage.DATABASE_ERROR);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
+    }
+
+}
