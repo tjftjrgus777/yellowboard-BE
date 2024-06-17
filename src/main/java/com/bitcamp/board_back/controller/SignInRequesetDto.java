123578765f3c13c07449e93bc7c6ad19f36340d2
tjftjrgus777 <tjftjrgus123@gmail.com>
2024-06-17 17:40:34 +0900

    Feat: 로그인 API 구현

---
+++ src/main/java/com/bitcamp/board_back/controller/SignInRequesetDto.java
@@ -0,0 +1,5 @@
+package com.bitcamp.board_back.controller;
+
+public class SignInRequesetDto {
+
+}
--- src/main/java/com/bitcamp/board_back/repository/UserRepository.java
+++ src/main/java/com/bitcamp/board_back/repository/UserRepository.java
@@ -11,5 +11,7 @@ public interface UserRepository extends JpaRepository<UserEntity, String>{
     boolean existsByEmail(String email);
     boolean existsByNickname(String nickname);
     boolean existsByTelNumber(String telNumber);
+
+    UserEntity findByEmail(String email);
     
 }
