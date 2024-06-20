d0d5f60d427029c964d3d7f58648de5d13388200
tjftjrgus777 <tjftjrgus123@gmail.com>
2024-06-20 11:08:19 +0900

    Feat: 최신 게시물 리스트 불러오기 API 구현

--- src/main/java/com/bitcamp/board_back/dto/object/BoardListItem.java
+++ src/main/java/com/bitcamp/board_back/dto/object/BoardListItem.java
@@ -1,5 +1,10 @@
 package com.bitcamp.board_back.dto.object;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.bitcamp.board_back.entity.BoardListViewEntity;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,4 +24,26 @@ public class BoardListItem {
     private String writerNickname;
     private String writerProfileImage;
     
+    public BoardListItem(BoardListViewEntity boardListViewEntity) {
+        this.boardNumber = boardListViewEntity.getBoardNumber();
+        this.title = boardListViewEntity.getTitle();
+        this.content = boardListViewEntity.getContent();
+        this.boardTitleImage = boardListViewEntity.getTitleImage();
+        this.favoriteCount = boardListViewEntity.getFavoriteCount();
+        this.commentCount = boardListViewEntity.getCommentCount();
+        this.viewCount = boardListViewEntity.getViewCount();
+        this.writeDateTime = boardListViewEntity.getWriteDatetime();
+        this.writerNickname = boardListViewEntity.getWriterNickname();
+        this.writerProfileImage = boardListViewEntity.getWriterProfileImage();
+
+    }
+
+    public static List<BoardListItem> getList(List<BoardListViewEntity> boardListViewEntities) {
+        List<BoardListItem> list = new ArrayList<>();
+        for (BoardListViewEntity boardListViewEntity: boardListViewEntities) {
+            BoardListItem boardListItem = new BoardListItem(boardListViewEntity);
+            list.add(boardListItem);
+        }
+        return list;
+    }
 }
--- src/main/java/com/bitcamp/board_back/repository/BoardListViewRepository.java
+++ src/main/java/com/bitcamp/board_back/repository/BoardListViewRepository.java
@@ -1,5 +1,7 @@
 package com.bitcamp.board_back.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.bitcamp.board_back.entity.BoardListViewEntity;
 
 @Repository
 public interface BoardListViewRepository extends JpaRepository<BoardListViewEntity, Integer>{
+
+    List<BoardListViewEntity> findByOrderByWriteDatetimeDesc();
     
 }
