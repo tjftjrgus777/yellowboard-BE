30009c3b9f4f4f6cfe095b5406a67d704e06898a
ssomal62 <wxy890@gmail.com>
2024-06-27 13:21:32 +0900

    Refactor: JWT 인증 필터 수정 및 통합
    - JwtAuthenticationFilter 클래스 수정하여 토큰 검증 및 인증 로직 통합
    - JwtProvider 클래스의 resolveToken 메서드 사용하여 토큰 추출
    - 토큰 검증 및 사용자 인증 설정 로직 추가
    - Redis를 이용한 블랙리스트 토큰 검증 로직 추가
    - 기존 JwtAuthenticationFilter 클래스의 doFilterInternal 메서드 개선

---
+++ src/main/java/com/bitcamp/board_back/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,64 @@
+package com.bitcamp.board_back.jwt;
+
+import com.bitcamp.board_back.exception.AuthException;
+import com.bitcamp.board_back.service.RedisService;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+import static com.bitcamp.board_back.common.enums.ApiStatus.BLACKLISTED_TOKEN;
+import static com.bitcamp.board_back.constant.AuthConstant.BLACK_LIST_KEY_PREFIX;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+
+    private final JwtProvider jwtProvider;
+    private final RedisService redisService;
+
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String userToken = jwtProvider.resolveToken(request);
+
+        // 토큰이 존재할 때만 검증 - 인증을 요구하지 않는 API에 대한 처리
+        if (userToken != null) {
+            checkIfTokenInvalidated(userToken);
+            jwtProvider.validateToken(userToken);
+
+            Claims userInfo = jwtProvider.getInfoFromToken(userToken);
+            setAuthentication(userInfo.getSubject());
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    public void setAuthentication(String email) {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        Authentication authentication = jwtProvider.createAuthentication(email);
+        context.setAuthentication(authentication);
+        SecurityContextHolder.setContext(context);
+    }
+
+    private void checkIfTokenInvalidated(String token) {
+        if (redisService.hasKey(BLACK_LIST_KEY_PREFIX + token)) {
+            throw new AuthException(BLACKLISTED_TOKEN);
+        }
+    }
+
+}
